10af4c4acf9ba1c808480931eafcd667fd8458fc
Kinan Turman <kinan.t@gmail.com>
2017-03-03 21:04:46 -0600

    first commit

---
+++ Assets/GVRAddon/Scripts/GVRInteractiveItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GVRInteractiveItem : MonoBehaviour, IGvrGazeResponder {
+
+    public Action OnPointerEnter;
+    public Action OnPointerExit;
+    public Action OnPointerClick;
+
+    // Implement IGvrGazeResponder
+    public void OnGazeEnter()
+    {
+        if (OnPointerEnter != null)
+        {
+            OnPointerEnter();
+        }
+    }
+
+    public void OnGazeExit()
+    {
+        if (OnPointerExit != null)
+        {
+            OnPointerExit();
+        }
+    }
+
+    public void OnGazeTrigger()
+    {
+        if (OnPointerClick != null)
+        {
+            OnPointerClick();
+        }
+    }
+}
---
+++ Assets/GVRAddon/Scripts/SampleInteractiveCube.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(GVRInteractiveItem))]
+public class SampleInteractiveCube : MonoBehaviour {
+
+    [SerializeField]
+    private GVRInteractiveItem m_GvrInteractiveItem;
+
+    private void OnEnable()
+    {
+        m_GvrInteractiveItem.OnPointerEnter += HandleEnter;
+        m_GvrInteractiveItem.OnPointerExit += HandleExit;
+        m_GvrInteractiveItem.OnPointerClick += HandleClick;
+    }
+
+    private void OnDisable()
+    {
+        m_GvrInteractiveItem.OnPointerEnter -= HandleEnter;
+        m_GvrInteractiveItem.OnPointerExit -= HandleExit;
+        m_GvrInteractiveItem.OnPointerClick -= HandleClick;
+    }
+
+    private void HandleEnter() {
+        Debug.Log("Pointer has entered!");
+    }
+
+    private void HandleExit() {
+        Debug.Log("Pointer has exited!");
+    }
+
+    private void HandleClick() {
+        Debug.Log("Pointer has been clicked!");
+    }
+}
